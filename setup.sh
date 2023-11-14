018a909ed00dc42f24473f33ec4c023f9a54aa11
Yangyang Fu <fuyy2008@gmail.com>
2023-11-14 09:19:36 -0600

    added setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # get current project path by using the file path
-# get the path of this file
-export PROJECT_PATH=$(cd `dirname $0`; pwd)
+# use the directory of this file as the project path
+export PROJECT_PATH=$(cd `dirname $BASH_SOURCE`; pwd)
 echo ${PROJECT_PATH}
 
 # carla version
@@ -8,5 +8,5 @@ export CARLA_VERSION=0.9.14
 
 # set up paths
 export LEADERBOARD_ROOT=${PROJECT_PATH}/leaderboard
-set SCENARIO_RUNNER_ROOT=${PROJECT_PATH}/scenario_runner
-set PYTHONPATH=${LEADERBOARD_ROOT};${SCENARIO_RUNNER_ROOT};${PYTHONPATH}
+export SCENARIO_RUNNER_ROOT=${PROJECT_PATH}/scenario_runner
+export PYTHONPATH=${PROJECT_PATH}:${LEADERBOARD_ROOT}:${SCENARIO_RUNNER_ROOT}:${PYTHONPATH}
