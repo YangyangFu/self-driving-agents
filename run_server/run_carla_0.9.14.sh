8232b7b84436c4a20a270c1d22d20b07b5fa3b7d
Yangyang Fu <fuyy2008@gmail.com>
2024-02-14 10:33:08 -0600

    update docker run script for 0.9.14

--- run_server/run_carla_0.9.14.sh
+++ run_server/run_carla_0.9.14.sh
@@ -1,9 +1,11 @@
 docker run \
+    --privileged \
+    --gpus all \
     -e SDL_VIDEODRIVER=x11 \
     -e DISPLAY=$DISPLAY \
     -e XAUTHORITY=$XAUTHORITY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $XAUTHORITY:$XAUTHORITY \
-    -it --gpus 'all,"capabilities=graphics,utility,display,video,compute"' \
+    -it \
     -p 2000-2002:2000-2002 carlasim/carla:0.9.14 \
     ./CarlaUE4.sh -quality-level=Low -fps=15 -windowed -ResX=600 -ResY=480
